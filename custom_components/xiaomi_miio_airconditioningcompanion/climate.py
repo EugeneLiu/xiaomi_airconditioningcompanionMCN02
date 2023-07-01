f224d61bc41f4ef0a7769c45b1a0415344494d1c
abu9 <abu9@users.noreply.github.com>
2023-07-01 21:17:36 +0800

    Update climate.py

--- custom_components/xiaomi_miio_airconditioningcompanion/climate.py
+++ custom_components/xiaomi_miio_airconditioningcompanion/climate.py
@@ -264,7 +264,7 @@ class XiaomiAirConditioningCompanion(ClimateEntity):
         if new_state is None:
             return
         await self._async_update_temp(new_state)
-        await self.async_write_ha_state()
+        self.async_write_ha_state()
 
     async def _async_power_sensor_changed(self, entity_id, old_state, new_state):
         """Handle power sensor changes."""
@@ -272,7 +272,7 @@ class XiaomiAirConditioningCompanion(ClimateEntity):
             return
 
         await self._async_update_power_state(new_state)
-        await self.async_write_ha_state()
+        self.async_write_ha_state()
 
     async def _try_command(self, mask_error, func, *args, **kwargs):
         """Call a AC companion command handling error messages."""
